784ad4b2e0114b169839aee6da9b6824923fc021
Trevor <alutiiq@comcast.net>
2020-08-11 22:03:59 -0700

    typos found and corrected

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const massive = require('massive');
 const session = require('express-session');
-const authCtrl = require('./controllers/authController')
+const authCtrl = require('./controllers/authController');
+const treasureCtrl = require('./controllers/treasureController');
+const auth = require('./middleware/authMiddleware');
 
 const app = express();
 const port = 4000;
@@ -27,5 +29,12 @@ app.use(
 );
 
 app.post('/auth/register', authCtrl.register);
+app.post('/auth/login', authCtrl.login);
+app.get('/auth/logout', authCtrl.logout);
+
+app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure);
+app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure);
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure);
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
 
 app.listen(port, () => console.log(`I hear you on port: ${port}`));
